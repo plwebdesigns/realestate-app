2bb43bd4d0f381c16a94251368b3a63f988a2c5d
Paul Longo <paullongo@outlook.com>
2026-07-18 16:35:02 -0400

    Swithced to top nav

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -30,7 +30,7 @@ class AppPanelProvider extends PanelProvider
             ->login()
             ->colors([
                 'primary' => Color::Amber,
-            ])
+            ])->topNavigation(true)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
